4ab352da5115faad1626bdb2c448756bb110e7b6
Pedro Victor <pedroacvictor@gmail.com>
2026-04-02 12:07:39 -0300

    tela de login

---
+++ src/scripts/login.js
@@ -0,0 +1,28 @@
+const username = document.querySelector('#username-login');
+const password = document.querySelector('#password-login');
+const button = document.querySelector('#button-login');
+
+button.addEventListener('click', event => {
+event.preventDefault();/*evita o comportamento padrão do botão, que é enviar o formulário e recarregar a página*/
+
+const username_value = username.value;
+const password_value = password.value;
+
+if (
+    username_value === '' ||
+    password_value === ''
+) {
+    alert('Preencha todos os campos');
+    return;
+};
+
+if ( 
+    username_value !== sessionStorage.getItem('username') ||
+    password_value !== sessionStorage.getItem('password')
+) {
+    alert('Usuário ou senha incorretos');
+    return;
+};
+
+alert(`Bem-vindo, ${sessionStorage.getItem('name')}!`); /*sessionStorage.getItem() é um método usado para recuperar um valor armazenado em sessionStorage, onde o parâmetro é a chave do valor que queremos recuperar. No exemplo acima, estamos recuperando o nome do usuário com a chave 'name' e exibindo uma mensagem de boas-vindas usando template literals (``) para inserir o valor recuperado na mensagem. */
+});
---
+++ src/scripts/register.js
@@ -0,0 +1,30 @@
+const name = document.querySelector('#name-register'); /*# id e . se for classe*/
+const username = document.querySelector('#username-register');
+const password = document.querySelector('#password-register');
+const button = document.querySelector('#button-register');
+
+button.addEventListener('click', event => {
+event.preventDefault();/*evita o comportamento padrão do botão, que é enviar o formulário e recarregar a página*/
+
+const name_value = name.value;
+const username_value = username.value;
+const password_value = password.value;
+
+if (
+    name_value === '' || 
+    username_value === '' ||
+    password_value === ''
+) {
+    alert('Preencha todos os campos');
+    return;
+};
+
+sessionStorage.setItem('name', name_value); /*sessionStorage é uma forma de armazenar dados no navegador, ele armazena os dados apenas durante a sessão do navegador, ou seja, quando o navegador é fechado, os dados são apagados. Ele é útil para armazenar dados temporários, como o nome do usuário, que pode ser usado para exibir uma mensagem de boas-vindas na página de login. O método setItem() é usado para armazenar um valor em sessionStorage, onde o primeiro parâmetro é a chave e o segundo parâmetro é o valor. No exemplo acima, estamos armazenando o nome do usuário com a chave 'name' e o valor da variável name_value. */
+sessionStorage.setItem('username', username_value);
+sessionStorage.setItem('password', password_value);
+
+alert('Usuário cadastrado com sucesso!');
+
+window.location.href = './login.html'; /*redireciona para a página de login*/
+
+});
